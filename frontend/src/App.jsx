2ce51177c91e1edb55bbfc53fba33489d37f6ec5
mmohadIGN <melodia.mohad@ign.fr>
2023-04-03 15:57:52 +0200

    add control layer #13

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,44 @@
 import React, { useEffect } from "react";
-import { MapContainer, TileLayer} from "react-leaflet";
-import 'leaflet/dist/leaflet.css';
+import {
+  MapContainer,
+  WMSTileLayer,
+  TileLayer,
+  LayersControl,
+  LayerGroup,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 function App() {
   useEffect(() => {}, []);
+  const center = [47.390387412304094, 2.3483664012334238];
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+    <MapContainer center={center} zoom={6} scrollWheelZoom={false}>
+      <LayersControl position="topright">
+        <LayersControl.BaseLayer checked name="IGN topo">
+          <LayerGroup>
+            <WMSTileLayer
+              url="https://wxs.ign.fr/essentiels/geoportail/r/wms?'"
+              params={{
+                layers: "GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2",
+                continuousWorld: true,
+              }}
+            />
+          </LayerGroup>
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="Satellites">
+          <LayerGroup>
+            <WMSTileLayer
+              url='https://wxs.ign.fr/essentiels/geoportail/r/wms?'
+              params={{
+                layers: 'ORTHOIMAGERY.ORTHOPHOTOS',
+                continuousWorld: true,
+              }}
+              
+            />
+          </LayerGroup>
+        </LayersControl.BaseLayer>
+      </LayersControl>
     </MapContainer>
   );
 }
