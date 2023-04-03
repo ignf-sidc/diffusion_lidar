71a8fae86ec93760f01ebfeca7dac5578987b331
mmohadIGN <melodia.mohad@ign.fr>
2023-04-03 11:27:08 +0200

    add map #1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import { MapContainer, TileLayer} from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+
+function App() {
+  useEffect(() => {}, []);
+
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
+}
+
+export default App;
