5b5398cdab21b7d98575c48118a4934ee9ea0321
LArdilly <louis.ardilly@ign.fr>
2023-09-07 14:15:49 +0200

    remove migration

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 # python3 ./api/app/adapters/drop_data.py
 
-python3 ./api/app/adapters/migration.py
+# python3 ./api/app/adapters/migration.py
 
 uvicorn api.app.main:app --host 0.0.0.0 --port 8000 --reload
