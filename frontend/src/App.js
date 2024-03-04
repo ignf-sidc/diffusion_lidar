eb2694b9e9feadd2cb30fbc2295a2a64feab6adc
mmohad <melodia.mohad@ign.fr>
2024-03-04 13:00:38 +0100

    Supress cookies #15

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,6 @@ import "../node_modules/ol/ol.css";
 import { FaTimes, FaMapMarker } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { BsChevronDown, BsChevronLeft } from "react-icons/bs";
-import { withCookies } from "react-cookie";
 import {
   Card,
   Radio,
@@ -45,7 +44,6 @@ class App extends Component {
     super(props);
     // on défini un state pour savoir quand ouvrir la modal d'information TODO: Reprendre les définitions de state
 
-    const { cookies } = props;
     this.state = {
       isModalOpen: false,
       coordinate: null,
@@ -58,16 +56,12 @@ class App extends Component {
       selectedMode: "click",
       zoom: 5,
       coor_mouse: null,
-      expiresDateCookie: null,
-      cookie_zoom_start: cookies.get("zoom") || 6,
-      cookie_coor_start: cookies.get("coor") || [
-        288074.8449901076, 6247982.515792289,
-      ],
+      zoom_start: 6,
+      coor_start: [288074.8449901076, 6247982.515792289],
       api_url: null,
       fileUpload: [],
     };
     this.name_file_txt = "liste_dalle.txt";
-    this.day_cookie_expiration = 7;
     this.dalles_select = [];
     this.polygon_drawn = [];
     this.limit_dalle_select = 2500;
@@ -601,11 +595,6 @@ class App extends Component {
     );
     register(proj4);
 
-    const expiresDate = new Date();
-    expiresDate.setDate(expiresDate.getDate() + this.day_cookie_expiration);
-    // expiration des cookies
-    this.setState({ expiresDateCookie: expiresDate });
-
     var createMap = () => {
       var map = new Map({
         target: "map",
@@ -624,8 +613,8 @@ class App extends Component {
         ],
         view: new View({
           projection: getProjection("EPSG:2154"),
-          center: this.state.cookie_coor_start,
-          zoom: this.state.cookie_zoom_start,
+          center: this.state.coor_start,
+          zoom: this.state.zoom_start,
           maxZoom: 16,
         }),
       });
@@ -835,16 +824,6 @@ class App extends Component {
       map.on("moveend", () => {
         var view = map.getView();
         this.setState({ zoom: view.getZoom() });
-        // creation du cookie
-        const { cookies } = this.props;
-        // on set le cookie à chaque fois qu'on zoom
-        cookies.set("zoom", this.state.zoom, {
-          expires: this.state.expiresDateCookie,
-        });
-        // on set le cookie avec les coordonnées à chaque fois qu'on bouge sur la carte
-        cookies.set("coor", view.getCenter(), {
-          expires: this.state.expiresDateCookie,
-        });
         // recupere la bbox de la fenetre de son pc
         var extent = view.calculateExtent(map.getSize());
 
@@ -1220,4 +1199,4 @@ class App extends Component {
   }
 }
 
-export default withCookies(App);
+export default App;
