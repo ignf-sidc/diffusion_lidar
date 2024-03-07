ea92a9d1a80d07f1ef2a922ce0e6d9e47afaf6d0
mmohad <melodia.mohad@ign.fr>
2024-03-07 11:11:34 +0100

    fix(modal): hidden cancel button #71

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1140,6 +1140,7 @@ class App extends Component {
                   open={this.state.isModalOpen}
                   onOk={this.handleOk}
                   width={650}
+                  cancelButtonProps={{hidden: true}}
                 >
                   <div>
                     <ul>
