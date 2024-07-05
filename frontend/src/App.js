2966118a5712a4e8df60351da3d73282e02e33ee
mmohad <melodia.mohad@ign.fr>
2024-07-05 10:12:12 +0200

    add(menu): tooltip for info user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import ol,{ View, Map, Overlay } from "ol";
+import ol, { View, Map, Overlay } from "ol";
 import axios from "axios";
 import Feature from "ol/Feature";
 import { Polygon, MultiPolygon } from "ol/geom";
@@ -24,6 +24,7 @@ import {
   Collapse,
   Popover,
   Modal,
+  Tooltip,
 } from "antd";
 import {
   UploadOutlined,
@@ -718,25 +719,24 @@ class App extends Component {
         condition: function (event) {
           return platformModifierKeyOnly(event) && click(event);
         },
-        layers: [this.vectorLayer]
-      })
+        layers: [this.vectorLayer],
+      });
 
-      this.visionneuseInteraction.on("select", (event) =>{
-
-        event.preventDefault()
-          const featureSelect = event.selected[0]
-          const a = document.createElement("a")
-          a.href =`${this.state.api_url}/visionneuse?copc=`.concat(event.selected[0].values_.properties.url_download)
-          a.setAttribute("target","_blank")
-          a.click()
-
-          
-      })
-      map.addInteraction(this.visionneuseInteraction)
+      this.visionneuseInteraction.on("select", (event) => {
+        event.preventDefault();
+        const featureSelect = event.selected[0];
+        const a = document.createElement("a");
+        a.href = `${this.state.api_url}/visionneuse?copc=`.concat(
+          event.selected[0].values_.properties.url_download
+        );
+        a.setAttribute("target", "_blank");
+        a.click();
+      });
+      map.addInteraction(this.visionneuseInteraction);
 
       this.selectInteractionClick = new Select({
         condition: function (event) {
-          return click(event) && !platformModifierKeyOnly(event)
+          return click(event) && !platformModifierKeyOnly(event);
         },
         layers: [this.vectorLayer],
       });
@@ -1119,30 +1119,41 @@ class App extends Component {
         </div>
 
         <div className="menu">
-          <div className="dalle-select">
+          <div className="menu-interface">
             <Space>
               <Button href="/" disabled="true">
                 Nuage de point
               </Button>
-              <Button href="/MNX/">Produit dérivé</Button>
+              <Tooltip title="Accéder à la page de téléchargement des produits dérivées LiDAR-HD">
+                <Button href="/mnx/">Produit dérivé</Button>
+              </Tooltip>
             </Space>
-            {this.state.zoom >= this.zoom_dispaly_dalle ? (
-              <div className="menu_mode">
-                <Card title="Choix du mode de sélection">
-                  <Space
-                    direction="vertical"
-                    style={{ width: "100%" }}
-                    size="large"
+          </div>
+          {this.state.zoom >= this.zoom_dispaly_dalle ? (
+            <div className="menu_mode">
+              <Card
+                title="Choix du mode de sélection"
+                extra={
+                  <Tooltip title="En mode clic, pour visualiser une dalle faites ctrl+clic sur la dalle">
+                    <QuestionCircleOutlined />
+                  </Tooltip>
+                }
+              >
+                <Space
+                  direction="vertical"
+                  style={{ width: "100%" }}
+                  size="large"
+                >
+                  <Radio.Group
+                    onChange={this.handleModeChange}
+                    value={this.state.selectedMode}
                   >
-                    <Radio.Group
-                      onChange={this.handleModeChange}
-                      value={this.state.selectedMode}
-                    >
-                      <Radio value={"click"}>Clic</Radio>
-                      <Radio value={"polygon"}>Polygone</Radio>
-                      <Radio value={"rectangle"}>Rectangle</Radio>
-                    </Radio.Group>
-                    {/* <Upload
+                    <Radio value={"click"}>Clic</Radio>
+                    <Radio value={"polygon"}>Polygone</Radio>
+                    <Radio value={"rectangle"}>Rectangle</Radio>
+                  </Radio.Group>
+
+                  {/* <Upload
                     maxCount={1}
                     accept=".geojson"
                     action={`${this.state.api_url}/api/upload/geojson`}
@@ -1153,102 +1164,99 @@ class App extends Component {
                       Téléverser un GéoJSON (en lambert 93)
                     </Button>
                   </Upload> */}
-                  </Space>
-                </Card>
-                <br />
-                <Collapse items={items_collapse_liste_polygons}></Collapse>
-                <br />
-              </div>
-            ) : null}
-
-            {this.state.dalles_select.length === 0 ? (
-              <h3 className="center">Aucune donnée sélectionnée.</h3>
-            ) : (
-              <React.Fragment>
-                {this.state.dalles_select.length >= this.limit_dalle_select ? (
-                  <h5 className="text_red">
-                    Nombre de dalles sélectionnées :{" "}
-                    {this.state.dalles_select.length}/{this.limit_dalle_select}
-                  </h5>
-                ) : (
-                  <h5>
-                    Nombre de dalles sélectionnées :{" "}
-                    {this.state.dalles_select.length}/{this.limit_dalle_select}
-                  </h5>
-                )}
-                <Collapse items={items_collapse_liste_produit}></Collapse>
-              </React.Fragment>
-            )}
-          </div>
-
-          {this.state.dalles_select.length > 0 ? (
-            <div className="center">
-              <Space>
-                <Button
-                  onClick={this.handleTelechargement}
-                  type="default"
-                  icon={<DownloadOutlined />}
-                  size="large"
-                >
-                  Télécharger la liste des liens
-                </Button>
-                <Button
-                  type="primary"
-                  onClick={this.showModal}
-                  icon={<QuestionCircleOutlined />}
-                ></Button>
-                <Modal
-                  title="Comment télécharger les données ?"
-                  open={this.state.isModalOpen}
-                  onOk={this.handleOk}
-                  onCancel={this.handleCancel}
-                  width={650}
-                  cancelButtonProps={{ style: { display: "none" } }}
-                >
-                  <div>
-                    <ul>
-                      <li>
-                        Cette interface vous permet de récupérer la liste des
-                        données vous intéressent.
-                      </li>
-                      <li>
-                        Pour récupérer les données facilement, il faudra
-                        automatiser le téléchargement.
-                      </li>
-                      <li>
-                        Pour cela, vous pouvez utiliser les applicatifs suivants
-                        (par exemple) :
-                      </li>
-                      <ul>
-                        <li>
-                          <a href="https://www.downthemall.net/">
-                            DownThemAll!
-                          </a>
-                        </li>
-                        <li>
-                          <a href="https://xtremedownloadmanager.com/">
-                            Xtreme Download Manager
-                          </a>
-                        </li>
-                      </ul>
-                    </ul>
-                    Pour plus d'explications la vidéo suivante vous indique la
-                    marche à suivre :
-                    <iframe
-                      width="560"
-                      height="315"
-                      src="https://www.youtube.com/embed/-YomQJC6S38?si=ycCCdLbQ4KmMNSqn&amp;start=59"
-                      title="YouTube video player"
-                      frameborder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                      allowfullscreen
-                    ></iframe>
-                  </div>
-                </Modal>
-              </Space>
+                </Space>
+              </Card>
+              <br />
+              <Collapse items={items_collapse_liste_polygons}></Collapse>
+              <br />
             </div>
           ) : null}
+
+          {this.state.dalles_select.length === 0 ? (
+            <h3 className="center">Aucune donnée sélectionnée.</h3>
+          ) : (
+            <React.Fragment>
+              {this.state.dalles_select.length >= this.limit_dalle_select ? (
+                <h5 className="text_red">
+                  Nombre de dalles sélectionnées :{" "}
+                  {this.state.dalles_select.length}/{this.limit_dalle_select}
+                </h5>
+              ) : (
+                <h5>
+                  Nombre de dalles sélectionnées :{" "}
+                  {this.state.dalles_select.length}/{this.limit_dalle_select}
+                </h5>
+              )}
+              <Collapse items={items_collapse_liste_produit}></Collapse>
+            </React.Fragment>
+          )}
         </div>
+
+        {this.state.dalles_select.length > 0 ? (
+          <div className="center">
+            <Space>
+              <Button
+                onClick={this.handleTelechargement}
+                type="default"
+                icon={<DownloadOutlined />}
+                size="large"
+              >
+                Télécharger la liste des liens
+              </Button>
+              <Button
+                type="primary"
+                onClick={this.showModal}
+                icon={<QuestionCircleOutlined />}
+              ></Button>
+              <Modal
+                title="Comment télécharger les données ?"
+                open={this.state.isModalOpen}
+                onOk={this.handleOk}
+                onCancel={this.handleCancel}
+                width={650}
+                cancelButtonProps={{ style: { display: "none" } }}
+              >
+                <div>
+                  <ul>
+                    <li>
+                      Cette interface vous permet de récupérer la liste des
+                      données vous intéressent.
+                    </li>
+                    <li>
+                      Pour récupérer les données facilement, il faudra
+                      automatiser le téléchargement.
+                    </li>
+                    <li>
+                      Pour cela, vous pouvez utiliser les applicatifs suivants
+                      (par exemple) :
+                    </li>
+                    <ul>
+                      <li>
+                        <a href="https://www.downthemall.net/">DownThemAll!</a>
+                      </li>
+                      <li>
+                        <a href="https://xtremedownloadmanager.com/">
+                          Xtreme Download Manager
+                        </a>
+                      </li>
+                    </ul>
+                  </ul>
+                  Pour plus d'explications la vidéo suivante vous indique la
+                  marche à suivre :
+                  <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/-YomQJC6S38?si=ycCCdLbQ4KmMNSqn&amp;start=59"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                  ></iframe>
+                </div>
+              </Modal>
+            </Space>
+          </div>
+        ) : null}
 
         {this.state.coor_mouse !== null ? (
           <Card bodyStyle={{ padding: "2px" }} className="coor">
