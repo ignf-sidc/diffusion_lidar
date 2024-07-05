2bee8a38a61b0b0b04141c638a7416c369b97c33
mmohad <melodia.mohad@ign.fr>
2024-07-05 14:59:12 +0200

    fix(menu): ortho and style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1122,23 +1122,16 @@ class App extends Component {
           <div className="menu-interface">
             <Space>
               <Button href="/" disabled="true">
-                Nuage de point
+                Nuages de points
               </Button>
-              <Tooltip title="Accéder à la page de téléchargement des produits dérivées LiDAR-HD">
-                <Button href="/mnx/">Produit dérivé</Button>
+              <Tooltip title="Accéder à la page de téléchargement des produits dérivés LiDAR HD">
+                <Button href="/mnx/">Produits dérivés</Button>
               </Tooltip>
             </Space>
           </div>
           {this.state.zoom >= this.zoom_dispaly_dalle ? (
             <div className="menu_mode">
-              <Card
-                title="Choix du mode de sélection"
-                extra={
-                  <Tooltip title="En mode clic, pour visualiser une dalle faites ctrl+clic sur la dalle">
-                    <QuestionCircleOutlined />
-                  </Tooltip>
-                }
-              >
+              <Card title="Choix du mode de sélection">
                 <Space
                   direction="vertical"
                   style={{ width: "100%" }}
@@ -1152,7 +1145,6 @@ class App extends Component {
                     <Radio value={"polygon"}>Polygone</Radio>
                     <Radio value={"rectangle"}>Rectangle</Radio>
                   </Radio.Group>
-
                   {/* <Upload
                     maxCount={1}
                     accept=".geojson"
@@ -1172,91 +1164,95 @@ class App extends Component {
             </div>
           ) : null}
 
-          {this.state.dalles_select.length === 0 ? (
-            <h3 className="center">Aucune donnée sélectionnée.</h3>
-          ) : (
-            <React.Fragment>
-              {this.state.dalles_select.length >= this.limit_dalle_select ? (
-                <h5 className="text_red">
-                  Nombre de dalles sélectionnées :{" "}
-                  {this.state.dalles_select.length}/{this.limit_dalle_select}
-                </h5>
-              ) : (
-                <h5>
-                  Nombre de dalles sélectionnées :{" "}
-                  {this.state.dalles_select.length}/{this.limit_dalle_select}
-                </h5>
-              )}
-              <Collapse items={items_collapse_liste_produit}></Collapse>
-            </React.Fragment>
-          )}
-        </div>
+          <div className="dalle-select">
+            {this.state.dalles_select.length === 0 ? (
+              <h3 className="center">Aucune donnée sélectionnée.</h3>
+            ) : (
+              <React.Fragment>
+                {this.state.dalles_select.length >= this.limit_dalle_select ? (
+                  <h5 className="text_red">
+                    Nombre de dalles sélectionnées :{" "}
+                    {this.state.dalles_select.length}/{this.limit_dalle_select}
+                  </h5>
+                ) : (
+                  <h5>
+                    Nombre de dalles sélectionnées :{" "}
+                    {this.state.dalles_select.length}/{this.limit_dalle_select}
+                  </h5>
+                )}
+                <Collapse items={items_collapse_liste_produit}></Collapse>
+              </React.Fragment>
+            )}
+          </div>
 
-        {this.state.dalles_select.length > 0 ? (
-          <div className="center">
-            <Space>
-              <Button
-                onClick={this.handleTelechargement}
-                type="default"
-                icon={<DownloadOutlined />}
-                size="large"
-              >
-                Télécharger la liste des liens
-              </Button>
-              <Button
-                type="primary"
-                onClick={this.showModal}
-                icon={<QuestionCircleOutlined />}
-              ></Button>
-              <Modal
-                title="Comment télécharger les données ?"
-                open={this.state.isModalOpen}
-                onOk={this.handleOk}
-                onCancel={this.handleCancel}
-                width={650}
-                cancelButtonProps={{ style: { display: "none" } }}
-              >
-                <div>
-                  <ul>
-                    <li>
-                      Cette interface vous permet de récupérer la liste des
-                      données vous intéressent.
-                    </li>
-                    <li>
-                      Pour récupérer les données facilement, il faudra
-                      automatiser le téléchargement.
-                    </li>
-                    <li>
-                      Pour cela, vous pouvez utiliser les applicatifs suivants
-                      (par exemple) :
-                    </li>
+          {this.state.dalles_select.length > 0 ? (
+            <div className="center">
+              <Space>
+                <Button
+                  onClick={this.handleTelechargement}
+                  type="default"
+                  icon={<DownloadOutlined />}
+                  size="large"
+                >
+                  Télécharger la liste des liens
+                </Button>
+                <Button
+                  type="primary"
+                  onClick={this.showModal}
+                  icon={<QuestionCircleOutlined />}
+                ></Button>
+                <Modal
+                  title="Comment télécharger les données ?"
+                  open={this.state.isModalOpen}
+                  onOk={this.handleOk}
+                  onCancel={this.handleCancel}
+                  width={650}
+                  cancelButtonProps={{ style: { display: "none" } }}
+                >
+                  <div>
                     <ul>
                       <li>
-                        <a href="https://www.downthemall.net/">DownThemAll!</a>
+                        Cette interface vous permet de récupérer la liste des
+                        données vous intéressent.
                       </li>
                       <li>
-                        <a href="https://xtremedownloadmanager.com/">
-                          Xtreme Download Manager
-                        </a>
+                        Pour récupérer les données facilement, il faudra
+                        automatiser le téléchargement.
                       </li>
+                      <li>
+                        Pour cela, vous pouvez utiliser les applicatifs suivants
+                        (par exemple) :
+                      </li>
+                      <ul>
+                        <li>
+                          <a href="https://www.downthemall.net/">
+                            DownThemAll!
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://xtremedownloadmanager.com/">
+                            Xtreme Download Manager
+                          </a>
+                        </li>
+                      </ul>
                     </ul>
-                  </ul>
-                  Pour plus d'explications la vidéo suivante vous indique la
-                  marche à suivre :
-                  <iframe
-                    width="560"
-                    height="315"
-                    src="https://www.youtube.com/embed/-YomQJC6S38?si=ycCCdLbQ4KmMNSqn&amp;start=59"
-                    title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowfullscreen
-                  ></iframe>
-                </div>
-              </Modal>
-            </Space>
-          </div>
-        ) : null}
+                    Pour plus d'explications la vidéo suivante vous indique la
+                    marche à suivre :
+                    <iframe
+                      width="560"
+                      height="315"
+                      src="https://www.youtube.com/embed/-YomQJC6S38?si=ycCCdLbQ4KmMNSqn&amp;start=59"
+                      title="YouTube video player"
+                      frameborder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      allowfullscreen
+                    ></iframe>
+                  </div>
+                </Modal>
+              </Space>
+            </div>
+          ) : null}
+        </div>
 
         {this.state.coor_mouse !== null ? (
           <Card bodyStyle={{ padding: "2px" }} className="coor">
